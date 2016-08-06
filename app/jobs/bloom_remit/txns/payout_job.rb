86b20fc597085805cb1118551c7907b87bb76c1a
Ramon Tayag <ramon.tayag@gmail.com>
2016-08-06 09:59:32 +0800

    Use full name to avoid fetching the wrong model

--- app/jobs/bloom_remit/txns/payout_job.rb
+++ app/jobs/bloom_remit/txns/payout_job.rb
@@ -6,7 +6,7 @@ module BloomRemit
       sidekiq_options retry: false
 
       def perform(txn_id)
-        txn = Txn.find(txn_id)
+        txn = ::BloomRemit::Txn.find(txn_id)
         Payout.(txn)
       end
 
